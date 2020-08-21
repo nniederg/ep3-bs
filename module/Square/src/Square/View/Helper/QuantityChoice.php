388f6b5dc14c1eb8e8e6784420a33498634b3702
nniederg <69654422+nniederg@users.noreply.github.com>
2020-08-21 20:10:14 +0200

    Update QuantityChoice.php

--- module/Square/src/Square/View/Helper/QuantityChoice.php
+++ module/Square/src/Square/View/Helper/QuantityChoice.php
@@ -33,7 +33,7 @@ class QuantityChoice extends AbstractHelper
 
         $html .= '<select id="sb-quantity" style="min-width: 64px;">';
 
-        for ($i = 1; $i <= $quantityAvailable; $i++) {
+        for ($i = 2; $i <= $quantityAvailable; $i++) {
             $html .= sprintf('<option value="%1$s">%1$s</option>', $i);
         }
 
